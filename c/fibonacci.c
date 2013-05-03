4af69bdd5e6cfd7e9961a4bbb41dbdd47136ebf7
Sean Fao <sean.fao@gmail.com>
2013-05-02 22:24:24 -0400

    Added app that calculates the sum of all of the even-valued terms in the Fibonacci sequence below four million.

---
+++ c/fibonacci.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <limits.h>
+
+int main(void)
+{
+  int i,
+      first  = 1,
+      previous = 1,
+      next = 1,
+      total  = 0;
+
+  for (i = 0; i < INT_MAX; i++)
+  {
+    printf("%d, ", next);
+
+    if (first + previous > 4000000)
+    {
+      break;
+    }
+
+    next = first + previous;
+    first = previous;
+    previous = next;
+
+    if (!(next % 2))
+    {
+      total += next;
+    }
+  }
+
+  printf("\n");
+  printf("%d\n", total);
+
+  return 0;
+}
