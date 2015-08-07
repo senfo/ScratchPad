0660273175c77b9e80814491b3b0dbb18d19e4c1
Sean <sean.fao@gmail.com>
2015-08-07 18:12:57 -0400

    Create reverse.c

---
+++ arduino/reverse.c
@@ -0,0 +1,55 @@
+/*
+    A really simple project, which demonstrates how you could use the Ardunio to build something similar to the
+    reverse sensor on a vehicle.
+*/
+
+#define TRIG 4
+#define ECHO 5
+#define BUZZER 13
+
+long duration;
+long distance;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(TRIG, OUTPUT);
+  pinMode(ECHO, INPUT);
+  pinMode(BUZZER, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(TRIG, LOW);
+  delayMicroseconds(2);
+  
+  digitalWrite(TRIG, HIGH);
+  delayMicroseconds(10);
+  
+  digitalWrite(TRIG, LOW);
+  duration = pulseIn(ECHO, HIGH);
+  distance = durationToInches(duration);
+  
+  if (distance > 9) {
+    noTone(BUZZER);
+  }
+  else if (distance <= 9 && distance > 6) {
+    tone(BUZZER, 1000, 100);
+  }
+  else if (distance <= 6 && distance > 3) {
+    tone(BUZZER, 1000, 50);
+  }
+  else if (distance <= 3) {
+    tone(BUZZER, 1000);
+  }
+  
+  Serial.println(distance);
+  
+  delay(100);
+}
+
+long durationToInches(long duration) {
+  return duration / 73.746 / 2;
+}
+
+long durationToCm(long duration) {
+  return duration / 58.2;
+}
