fcad78c04803f51f8f22bb558734600e69b99bb6
Sean <sean.fao@gmail.com>
2018-12-20 20:01:17 +0000

    Added a function to count the discrete left/right nodes.

--- c/depth.c
+++ c/depth.c
@@ -1,23 +1,35 @@
 #include <stdio.h>
 
+#define MAX(X, Y) (((X) > (Y)) ? (X) : (Y))
+
 typedef struct Node {
   struct Node *left;
   struct Node *right;
 } Node;
 
-int maxDepth(Node);
+typedef struct NodeDepth {
+  int left;
+  int right;
+} NodeDepth;
+
+int maxDepth(Node *);
+NodeDepth discreteDepth(Node);
 
 int main(void)
 {
   int depth;
   Node root;
-  Node a, b, c;
+  Node a, b, c, d;
+  NodeDepth nodeDepth;
+  
+  d.left = NULL;
+  d.right = NULL;
   
   c.left = NULL;
   c.right = NULL;
   
   b.left = &c;
-  b.right = NULL;
+  b.right = &d;
   
   a.left = NULL;
   a.right = &b;
@@ -25,28 +37,49 @@ int main(void)
   root.left = NULL;
   root.right = &a;
 
-  depth = maxDepth(root);
-  printf("%d\n", depth);
+  depth = maxDepth(&root);
+  nodeDepth = discreteDepth(root);
+  
+  printf("Maximum Depth: %d  -  Left Depth: %d  -  Right Depth: %d\n", depth, nodeDepth.left, nodeDepth.right);
 
   return 0;
 }
 
-int maxDepth(Node node)
+int maxDepth(Node *node)
 {
-  int left = 0;
-  int right = 0;
+  if (node == NULL)
+  {
+    return 0;
+  }
+  
+  return 1 + MAX(maxDepth(node->left), maxDepth(node->right));
+}
+
+NodeDepth discreteDepth(Node node)
+{
+  NodeDepth aggregate;
+  NodeDepth temp;
+  
+  aggregate.left = 0;
+  aggregate.right = 0;
 
   if (node.left != NULL)
   {
-    left++;
-    left += maxDepth(*node.left);
-  }
+    temp = discreteDepth(*node.left);
 
+    aggregate.left++;
+    aggregate.left += temp.left;
+    aggregate.right += temp.right;
+  }
+  
   if (node.right != NULL)
   {
-    right++;
-    right += maxDepth(*node.right);
+    temp = discreteDepth(*node.right);
+
+    aggregate.right++;
+    aggregate.right += temp.right;
+    aggregate.left += temp.left;
   }
 
-  return left > right ? left : right;
+  return aggregate;
 }
