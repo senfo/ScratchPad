a7f9361c22a4d981584d57b18b904626f1111f10
Sean <sean.fao@gmail.com>
2022-01-21 10:02:52 -0700

    Create inputs.sh

---
+++ bash/inputs.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+declare -a parameters=()
+declare -A flags=( ["u"]="RGT user name" ["p"]="RGT user password" ["r"]="Role" ["U"]="Postgres user" ["P"]="Postgres password" ["H"]="Postgres host" )
+
+usage () {
+    for flag in "${!flags[@]}"; do
+        echo "-$flag: ${flags[$flag]}"
+    done
+}
+
+# Why did I do this?
+show () {
+    for parameter in ${!parameters[@]}; do
+        echo ${parameters[$parameter]}
+    done
+}
+
+for flag in "${!flags[@]}"; do
+    parameters+=($flag)
+done
+
+# Add a parameter to handle help
+parameters+=(\?)
+
+# Join the parameters array, seperating by comma
+options=$(IFS=: ; echo "${parameters[*]}")
+#echo $options
+
+while getopts ${options} flag
+do
+    case "${flag}" in
+        u) username=${OPTARG};;
+        p) password=${OPTARG};;
+        r) role=${OPTARG};;
+        \?) usage; exit;;
+    esac
+done
+
+if [ -z ${username} ]; then
+    echo "Username required"
+    exit
+fi
+
+if [ -z ${role} ]; then
+    echo "Role required";
+    exit
+fi
+
+if [ -z ${password} ]; then
+    echo -n "User password: "
+    read -s password
+fi
+
+echo "User: ${username}  -  Password: ${password}  -  Role: ${role}"
