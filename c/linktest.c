30d7658e573456d8d23dc54b96b1b1d0c5d624a8
Fao, Sean <s.fao@astroscale-us.com>
2024-05-16 14:54:51 -0600

    Created a solution to the linked cycle test.

---
+++ c/linktest.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+typedef struct Node {
+    int value;
+    struct Node *next;
+} Node;
+
+int hasCycle(Node *head);
+
+int main(void)
+{
+    Node a;
+    Node b;
+    Node c;
+    
+    a.value = 1;
+    a.next = &b;
+    
+    b.value = 2;
+    b.next = &c;
+    
+    c.value = 3;
+    c.next = &a;
+
+    printf("Is circular? %s\n", hasCycle(&a) == 0 ? "No" : "Yes");
+    
+    return 0;
+}
+
+int hasCycle(Node *head)
+{
+    Node *slow = head;
+    Node *fast = head;
+
+    while (fast != NULL && fast->next != NULL)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+
+        if (slow == fast)
+	{
+            return 1; // There's a cycle
+        }
+    }
+
+    return 0; // No cycle found
+}
+
